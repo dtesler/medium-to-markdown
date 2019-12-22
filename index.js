0ca1fe8606018ab332e96683ee96ea69fc8a84c9
almenon <almenon214@gmail.com>
2019-12-22 15:11:27 -0800

    add ability to run from command line

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 'use strict';
 
+const convertFromUrl = require('./lib/convertFromUrl') 
+
 module.exports = {
-  convertFromUrl: require('./lib/convertFromUrl')
+  convertFromUrl
+}
+
+// if run as cmd utility
+if (typeof require !== 'undefined' && require.main === module) {
+  if(process.argv.length < 3){
+    console.log('What url to convert?')
+    return
+  }
+  convertFromUrl(process.argv[2]).then(function (markdown) {
+    console.log(markdown); //=> Markdown content of medium post
+  });
 }
